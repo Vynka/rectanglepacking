32186a66b6f7000f6768f719de80f4f70492b60f
antalinmurnig <antalinmurnig@218f3f5e-ad3d-11de-98d4-27b68f3bc93f>
2009-09-29 22:42:23 +0000

    Sustituidos caracteres con tildes y demás por caracteres sin tilde. Corregido un pequeño error al formatear el código.

--- trunk/src/rpp/Rectangle.java
+++ trunk/src/rpp/Rectangle.java
@@ -3,31 +3,31 @@ package rpp;
 /**
  * [TODO] DESCRIPCION DETALLADA DE LA CLASE
  * 
- * @author Castor Miguel Pérez Melian
- * @author Alberto Cabrera Pérez
+ * @author Castor Miguel Perez Melian
+ * @author Alberto Cabrera Perez
  * @author Javier Luis Moreno Villena
- * @author Alejandro Tejera Pérez
+ * @author Alejandro Tejera Perez
  * @version 1.0
  * @since 1.0
  */
 public class Rectangle {
 	/**
-	 * Atributo b: Medida en unidades de la base del rectángulo.
+	 * Atributo b: Medida en unidades de la base del rectangulo.
 	 */
 	private int b;
 	/**
-	 * Atributo h: Medida en unidades de la altura del rectángulo.
+	 * Atributo h: Medida en unidades de la altura del rectangulo.
 	 */
 	private int h;
 
 	/**
-	 * Constructor: Recibe como parámetros la base y la altura y construye un
-	 * rectángulo de dichas dimensiones.
+	 * Constructor: Recibe como parametros la base y la altura y construye un
+	 * rectangulo de dichas dimensiones.
 	 * 
 	 * @param b
-	 *            Medida en unidades de la base del rectángulo.
+	 *            Medida en unidades de la base del rectangulo.
 	 * @param h
-	 *            Medida en unidades de la altura del rectángulo.
+	 *            Medida en unidades de la altura del rectangulo.
 	 */
 	public Rectangle(int b, int h) {
 		this.b = b;
@@ -35,7 +35,7 @@ public class Rectangle {
 	}
 
 	/**
-	 * Constructor por defecto: Construye un rectángulo de 0 x 0.
+	 * Constructor por defecto: Construye un rectangulo de 0 x 0.
 	 */
 	public Rectangle() {
 		this.b = 0;
@@ -43,47 +43,47 @@ public class Rectangle {
 	}
 
 	/**
-	 * Setter b: Establece la medida de la base del rectángulo.
+	 * Setter b: Establece la medida de la base del rectangulo.
 	 * 
 	 * @param b
-	 *            Medida en unidades de la base del rectángulo.
+	 *            Medida en unidades de la base del rectangulo.
 	 */
 	public void setBase(int b) {
 		this.b = b;
 	}
 
 	/**
-	 * Setter h: Establece la medida de la altura del rectángulo.
+	 * Setter h: Establece la medida de la altura del rectangulo.
 	 * 
 	 * @param h
-	 *            Medida en unidades de la altura del rectángulo.
+	 *            Medida en unidades de la altura del rectangulo.
 	 */
 	public void setHeight(int h) {
 		this.h = h;
 	}
 
 	/**
-	 * Getter b: Devuelve la base del rectángulo.
+	 * Getter b: Devuelve la base del rectangulo.
 	 * 
-	 * @return Medida en unidades de la base del rectángulo.
+	 * @return Medida en unidades de la base del rectangulo.
 	 */
 	public int getBase() {
 		return this.b;
 	}
 
 	/**
-	 * Getter h: Devuelve la altura del rectángulo.
+	 * Getter h: Devuelve la altura del rectangulo.
 	 * 
-	 * @return Medida en unidades de la altura del rectángulo.
+	 * @return Medida en unidades de la altura del rectangulo.
 	 */
 	public int getHeight() {
 		return this.h;
 	}
 
 	/**
-	 * Método toString: Devuelve un String representativo de un rectángulo.
+	 * Metodo toString: Devuelve un String representativo de un rectangulo.
 	 * 
-	 * @return String representativo del rectángulo "(base, altura)".
+	 * @return String representativo del rectangulo "(base, altura)".
 	 */
 	public String toString() {
 		return new String("(" + this.b + ", " + this.h + ")");
