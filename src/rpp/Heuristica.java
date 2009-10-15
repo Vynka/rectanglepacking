e6c937475671376b7c0d98ed4ee1eec0211df8fa
aletepe <aletepe@218f3f5e-ad3d-11de-98d4-27b68f3bc93f>
2009-10-15 09:06:21 +0000

    Estructura de la clase Heuristica hecha.

--- src/rpp/Heuristica.java
+++ src/rpp/Heuristica.java
@@ -1,5 +1,7 @@
 package rpp;
 
+import java.util.ArrayList;
+
 /**
  * @author Castor Miguel Perez Melian
  * @author Alberto Cabrera Perez
@@ -11,8 +13,82 @@ package rpp;
 
 public class Heuristica {
 	
-	public Heuristica() {
-		
+	/**
+	 * Lista de puntos factibles en los que colocar un rectangulo
+	 * Como punto inicial esta el (0, 0)
+	 */
+	private ArrayList<Point> points = new ArrayList(1);
+	
+	/**
+	 * Solucion del problema
+	 */
+	private Solution solution = null;
+	
+	/**
+	 * Constructor de la clase Heuristica. Recibe un objeto
+	 * solucion desde los que obtener las soluciones vecinas.
+	 * 
+	 * @param s
+	 *          Solucion "inicial"
+	 */
+	public Heuristica(Solution s) {
+	  Point origen = new Point(0, 0);
+	  this.points.add(origen);
+	  this.solution = s;
 	}
 	
+	/**
+	 * 
+	 * @return
+	 */
+	public Solution getSolution() {
+	  
+	}
+	
+	/**
+	 * 
+	 * @param last
+	 *          Solucion a partir de la cual obtener una nueva
+	 * @return
+	 */
+	public Solution getSolution(Solution last) {
+	  
+	}
+	
+	/**
+	 * 
+	 * @param i
+	 *          indice del nuevo punto a devolver
+	 * 
+	 * @return siguiente punto de la lista de puntos factibles
+	 */
+	private Point getNewPoint(int i) {
+	  
+	}
+	
+	/**
+	 * @param i
+	 *          indice del nuevo rectangulo
+	 * @return siguiente rectangulo a colocar
+	 */
+	private Rectangle getNewRectangle(int i) {
+	  
+	}
+	
+	/**
+	 * Busca un punto de la lista de puntos que minimize el criterio de colocacion de
+	 * rectangulos, es decir, que al colocar el rectangulo seleccionado sea (menor area,
+	 * menor numero de puntos ocultos,... por decidir)
+	 */
+	private void allocateRectangle() {
+	  
+	}
+	
+	/**
+	 * Reestructura la lista de puntos, eliminando el usados, asi como los ocultados, y
+	 * añadiendo los nuevos puntos creados con la eleccion tomada.
+	 */
+	private void managePoints() {
+	  
+	}
 }
