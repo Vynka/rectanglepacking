f246ae82c20c60bd3324638a80ab8fb9d92ce620
haside <haside@gmail.com>
2009-10-27 16:28:31 +0000

    Añadido menu y varias opciones al panel principal.

---
+++ src/rpp/OptionsMenu.java
@@ -0,0 +1,29 @@
+package rpp;
+
+import java.awt.FlowLayout;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+@SuppressWarnings("serial")
+public class OptionsMenu extends JMenuBar {
+	
+	JMenu optionBut;
+	JMenuItem mFile;
+	JMenuItem mProperties;
+	JMenuItem mExit;
+
+	
+	public OptionsMenu () {
+		mProperties = new JMenuItem("Properties");
+		mExit = new JMenuItem("Exit");
+
+		optionBut = new JMenu("Opciones");
+		optionBut.add(mProperties);
+		optionBut.add(mExit);
+
+		this.setLayout(new FlowLayout(FlowLayout.LEFT));
+		this.add(optionBut);
+	}
+
+}
