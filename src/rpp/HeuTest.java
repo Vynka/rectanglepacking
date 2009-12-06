418f16b0226688b742f64c8bc0becc50be393834
haside <haside@gmail.com>
2009-12-06 18:56:54 +0000

    Empezadas las pruebas, se esta realizando busqueda local ansiosa con el GRASP de inicializacion En desarrollo  el EA

--- src/rpp/HeuTest.java
+++ src/rpp/HeuTest.java
@@ -11,38 +11,156 @@ public class HeuTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Problem p = new Problem ("./datasets/C4/2.dat");
-		Heuristica h = new Heuristica(p, Heuristica.WASTE_EVAL);
-		
+		Problem p = null;
+		Heuristica h = null;
 		long meanT = 0;
 		long meanFobj = 0;
 		double meanWaste = 0;
 		
 		long t;
-		final int PRUEBAS = 100;
+		final int PRUEBAS = 200;
 		File f = null;
 		FileWriter obs = null;
 		BufferedWriter bw = null;
-		try {
-			f = new File("/home/alu3774/Pruebitatuntuncas.cas");
+		try {	
+			
+			f = new File("./Tests/GRASPS_LS_ANXIOUS_ONE_SWAP.txt");
 		    obs = new FileWriter(f);
 		    bw = new BufferedWriter(obs);
-			for (int i = 0; i < PRUEBAS; i++) {
-				t = - System.nanoTime();
-				h.GRASP(Heuristica.POND_GRASP);
-				h.localSearch(Heuristica.ONE_SWAP, Heuristica.ANXIOUS_SAMPLING, Heuristica.POND_EVAL);
-				//Metodo guay a evaluar
-				t += System.nanoTime();
-				meanT = (meanT + t) / 2;
-				meanFobj = (p.getSolution().getArea() + meanFobj) / 2;
-				meanWaste = ((double)p.getSolution().getObjF() / (double)p.getAreaRec()
-							+ meanWaste) / 2;
+		    for (int q = 1; q <= 7; q++) {
+			    for (int j = 1; j <= 3; j++) {
+			    	String dataset = "./datasets/C" + q + "/" + j + ".dat";
+					p = new Problem (dataset);
+					h = new Heuristica(p, Heuristica.WASTE_EVAL);
+					bw.write("+++++++++++++++++++++++++++++++++++++++++++");
+					bw.newLine();
+					bw.write(dataset + "   Optimal: " + p.getAreaRec());
+					bw.newLine();
+					bw.write("+++++++++++++++++++++++++++++++++++++++++++");
+					bw.newLine();
+					bw.write(PRUEBAS + " tests");
+					bw.newLine();
+	
+					bw.write("+++ AREA +++");
+					bw.newLine();
+					meanT = 0;
+					meanFobj = 0;
+					meanWaste = 0;
+					for (int i = 0; i < PRUEBAS; i++) {
+						t = - System.nanoTime();
+						h.GRASP(Heuristica.AREA_GRASP);
+						h.localSearch(Heuristica.ONE_SWAP, Heuristica.ANXIOUS_SAMPLING, Heuristica.AREA_EVAL);
+						//Metodo guay a evaluar
+						t += System.nanoTime();
+						meanT = (meanT + t) / 2;
+						meanFobj = (p.getSolution().getArea() + meanFobj) / 2;
+						meanWaste = ((double)p.getSolution().getObjF() / (double)p.getSolution().getArea()
+									+ meanWaste) / 2;
+					}
+					bw.newLine();
+					bw.write(meanT / 1000000 + " Obj " + meanFobj + " Waste " + meanWaste);
+					bw.newLine();
+					bw.write("------------------------------------------------------------");
+					bw.newLine();
+					
+					bw.write("+++ MIXED +++");
+					bw.newLine();
+					meanT = 0;
+					meanFobj = 0;
+					meanWaste = 0;
+					for (int i = 0; i < PRUEBAS; i++) {
+						t = - System.nanoTime();
+						h.GRASP(Heuristica.MIXED_GRASP);
+					h.localSearch(Heuristica.ONE_SWAP, Heuristica.ANXIOUS_SAMPLING, Heuristica.MIXED_EVAL);
+						//Metodo guay a evaluar
+						t += System.nanoTime();
+						meanT = (meanT + t) / 2;
+						meanFobj = (p.getSolution().getArea() + meanFobj) / 2;
+						meanWaste = ((double)p.getSolution().getObjF() / (double)p.getSolution().getArea()
+								+ meanWaste) / 2;
+					}
+					bw.newLine();
+					bw.write(meanT / 1000000 + " Obj " + meanFobj + " Waste " + meanWaste);
+					bw.newLine();
+					bw.write("------------------------------------------------------------");
+					bw.newLine();
+					/*
+					bw.write("+++ DIAGONAL +++");
+					bw.newLine();
+					meanT = 0;
+					meanFobj = 0;
+					meanWaste = 0;
+					for (int i = 0; i < PRUEBAS; i++) {
+						t = - System.nanoTime();
+						h.GRASP(Heuristica.DIAGONAL_GRASP);
+						//Metodo guay a evaluar
+						t += System.nanoTime();
+						meanT = (meanT + t) / 2;
+						meanFobj = (p.getSolution().getArea() + meanFobj) / 2;
+						meanWaste = ((double)p.getSolution().getObjF() / (double)p.getSolution().getArea()
+								+ meanWaste) / 2;
+					}
+					bw.newLine();
+					bw.write(meanT / 1000000 + " Obj " + meanFobj + " Waste " + meanWaste);
+					bw.newLine();
+					bw.write("------------------------------------------------------------");
+					bw.newLine();
+					*/
+					bw.write("+++ WASTE +++");
+					bw.newLine();
+					meanT = 0;
+					meanFobj = 0;
+					meanWaste = 0;
+					for (int i = 0; i < PRUEBAS; i++) {
+						t = - System.nanoTime();
+						h.GRASP(Heuristica.WASTE_GRASP);
+						h.localSearch(Heuristica.ONE_SWAP, Heuristica.ANXIOUS_SAMPLING, Heuristica.WASTE_EVAL);
+						//Metodo guay a evaluar
+						t += System.nanoTime();
+						meanT = (meanT + t) / 2;
+						meanFobj = (p.getSolution().getArea() + meanFobj) / 2;
+						meanWaste = ((double)p.getSolution().getObjF() / (double)p.getSolution().getArea()
+								+ meanWaste) / 2;
+					}
+					bw.newLine();
+					bw.write(meanT / 1000000 + " Obj " + meanFobj + " Waste " + meanWaste);
+					bw.newLine();
+					bw.write("------------------------------------------------------------");
+					bw.newLine();
+					
+					bw.write("+++ POND +++");
+					bw.newLine();
+					meanT = 0;
+					meanFobj = 0;
+					meanWaste = 0;
+					for (int i = 0; i < PRUEBAS; i++) {
+						t = - System.nanoTime();
+						h.GRASP(Heuristica.POND_GRASP);
+						h.localSearch(Heuristica.ONE_SWAP, Heuristica.ANXIOUS_SAMPLING, Heuristica.POND_EVAL);
+						//Metodo guay a evaluar
+						t += System.nanoTime();
+						meanT = (meanT + t) / 2;
+						meanFobj = (p.getSolution().getArea() + meanFobj) / 2;
+						meanWaste = ((double)p.getSolution().getObjF() / (double)p.getSolution().getArea()
+								+ meanWaste) / 2;
+					}
+					bw.newLine();
+					bw.write(meanT / 1000000 + " Obj " + meanFobj + " Waste " + meanWaste);
+					bw.newLine();
+					bw.write("------------------------------------------------------------");
+					bw.newLine();
+					bw.write("------------------------------------------------------------");
+					bw.newLine();
+					bw.write("------------------------------------------------------------");
+					bw.newLine();
+					bw.write("------------------------------------------------------------");
+					bw.newLine();
+					bw.write("------------------------------------------------------------");
+					bw.newLine();
+					
+					System.out.println(dataset);
+			    }
 			}
-			bw.newLine();
-			bw.newLine();
-			bw.newLine();
-			bw.write(meanT / 1000000 + " Obj " + meanFobj + " Waste " + meanWaste);
-			bw.newLine();
 		} catch (IOException e) {
 			e.printStackTrace();
 		} finally {
