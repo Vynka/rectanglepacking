39aacebf9be1bdb98cb95cf77d091e25e22d8ca3
haside <haside@gmail.com>
2009-12-09 21:55:07 +0000

    Como nadie da señales de vida he decidido que exponemos el Lunes 14.
    El que pueda genial.
    El que no, que se las apañe para poder y si hay alguna queja que lo diga (causa mayor).

--- src/rpp/Main.java
+++ src/rpp/Main.java
@@ -30,7 +30,7 @@ public class Main {
 	public static void main(String[] args) {
 		MainFrame mf = new MainFrame();
 		mf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mf.setVisible(true);
+		mf.setVisible(true);	
 	}
 	
 }
